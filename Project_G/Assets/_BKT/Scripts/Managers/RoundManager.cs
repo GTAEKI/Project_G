c760a012450d1e99e4c82ddd5549fe70fce67bfc
DESKTOP-BLPQKLT\BAE <kyungtaek0204@gmail.com>
2024-08-22 17:20:14 +0900

    [Fix] Enemy 스탯 조정

--- Project_G/Assets/_BKT/Scripts/Managers/RoundManager.cs
+++ Project_G/Assets/_BKT/Scripts/Managers/RoundManager.cs
@@ -8,17 +8,19 @@ public class RoundManager
 {
     public class RoundSetting
     {
-        public RoundSetting(int _maxEnemyCount, float _hpMultiple, float _powerMultiple, int scrapCount)
+        public RoundSetting(int _maxEnemyCount, float _hpMultiple, float _powerMultiple, float _speedMultiple, int scrapCount)
         {
             maxEnemyCount = _maxEnemyCount;
             enemyHpMultiple = _hpMultiple;
             enemyPowerMultiple = _powerMultiple;
+            enemySpeedMultiple = _powerMultiple;
             this.scrapCount = scrapCount;
         }
 
         public int maxEnemyCount;
         public float enemyHpMultiple;
         public float enemyPowerMultiple;
+        public float enemySpeedMultiple;
         public int scrapCount;
     }
 
@@ -26,13 +28,13 @@ public class RoundManager
     public int CurrentRound { get; private set; } = 0;
     public bool IsAllRoundClear { get; private set; } = false;
 
-    public RoundManager() 
+    public RoundManager()
     {
-        rounds.Add(new RoundSetting(20, 1f, 1f, 500));
-        rounds.Add(new RoundSetting(20, 1.5f, 1.1f, 700));
-        rounds.Add(new RoundSetting(25, 2f, 1.2f,800));
-        rounds.Add(new RoundSetting(30, 3f, 1.3f,1000));
-        rounds.Add(new RoundSetting(40, 4f, 1.4f, 1200));
+        rounds.Add(new RoundSetting(25, 1f, 1f, 1f, 500));
+        rounds.Add(new RoundSetting(30, 1.5f, 1.1f, 1.1f, 700));
+        rounds.Add(new RoundSetting(35, 2f, 1.2f, 1.2f, 800));
+        rounds.Add(new RoundSetting(40, 3f, 1.3f, 1.3f, 1000));
+        rounds.Add(new RoundSetting(45, 4f, 1.4f, 1.4f, 1200));
     }
 
     public RoundSetting GetCurrentRoundSetting() 
